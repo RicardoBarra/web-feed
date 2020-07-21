89dd79f28dcffdfcac29f92db6d68e42a7677ebd
RicardoBarra <rique_barra@hotmail.com>
2020-07-21 16:55:20 +0200

    fixed an issue with the post

--- src/components/Fetching.js
+++ src/components/Fetching.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 import Timeout from 'await-timeout'
 
 export default function ArticleList() {
+  const [show_comments, set_show_comments] = useState(false) 
   const [posts, set_posts] = useState([])
 
   useEffect(() => {
     async function waitForATimer() {
       
-      await Timeout.set(2000)
-      await axios.get("https://jsonplaceholder.typicode.com/posts")
+      await axios.get("https://jsonplaceholder.typicode.com/post/1/comments")
         .then(res => {
           set_posts(res.data)
           console.log(res)
@@ -21,20 +21,21 @@ export default function ArticleList() {
     waitForATimer();
   }, []);
 
+  const comment_section = posts ? posts.map(post => {
+    return(
+        <div key={post.id}>
+            <li key={post}>
+               {post.body}  {''}
+            </li>
+        </div>
+    )
+}) : 'Loading...'
  
   return (
      <div>
        <p>Here's a lovely list of articles, for your reading pleasure:</p>
-        <button onClick={() => set_posts([])}>Clear notifications</button> {''}
-             {posts ? posts.map(post => {
-                  return(
-                      <div key={post.id}>
-                          <li key={post}>
-                             {post.body}  {''}
-                          </li>
-                      </div>
-                  )
-              }) : 'Loading...'}
+        <button onClick={() => set_show_comments(!show_comments)}>Post</button> {''}
+         {show_comments ? comment_section : null}    
      </div>
   )
 }
